bda01bf29a471516452a41e992190a18e68d30b6
brian <2281033@unai.edu>
2025-04-06 10:19:35 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css' // kalau kamu pakai Tailwind
+import './style.css' // kalau kamu pakai Tailwind
 
 createApp(App).mount('#app')
